e568b806bb81d5d6a372f0a679bb5f9e2248b0c6
Hael12122002 <hendralijaya@gmail.com>
2022-08-22 15:51:42 +0700

    make user controller create all and find by id

--- controller/user-controller.go
+++ controller/user-controller.go
@@ -1,14 +1,18 @@
 package controller
 
 import (
+	"hendralijaya/user-management-project/helper"
+	"hendralijaya/user-management-project/model/web"
 	"hendralijaya/user-management-project/service"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type UserController interface {
 	All(context *gin.Context)
-	FindByID(context *gin.Context)
+	FindById(context *gin.Context)
 	Insert(context *gin.Context)
 	Update(context *gin.Context)
 	Delete(context *gin.Context)
@@ -24,4 +28,54 @@ func NewUserController(userService service.UserService, jwtService service.JWTSe
 		userService: userService,
 		jwtService: jwtService,
 	}
+}
+
+func (c *userController) All(context *gin.Context) {
+	users := c.userService.All()
+	webResponse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Errors: "",
+		Data:   users,
+	}
+	context.JSON(http.StatusOK, webResponse)
+}
+
+func (c *userController) FindByID(context *gin.Context) {
+	idString := context.Param("id")
+	id , err := strconv.ParseUint(idString, 10, 64)
+	user, err := c.userService.FindById(id)
+	ok := helper.NotFoundError(context, err)
+	if ok {
+		return
+	}
+	webResponse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Errors: "",
+		Data:   user,
+	}
+	context.JSON(http.StatusOK, webResponse)
+}
+
+func (c *userController) Insert(context *gin.Context) {
+	var u web.UserRegisterRequest
+	err := context.BindJSON(&u)
+	ok := helper.ValidationError(context, err)
+	if ok {
+		return
+	}
+	u.Role_id = 1
+	user, err := c.userService.Create(u)
+	ok = helper.InternalServerError(context, err)
+	if ok {
+		return
+	}
+	webResponse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Errors: "",
+		Data:   user,
+	}
+	context.JSON(http.StatusOK, webResponse)
 }
